19e6a594ce9d72a6907930653b93db2f3c56e80e
WhiteKr <hanjisu2215g@gmail.com>
2022-05-09 01:30:02 +0900

    feat: register commands on ready

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,8 +1,10 @@
 import { Client } from "discord.js";
+import { Commands } from "src/commands";
 
 export default (client: Client): void => {
     client.on('ready', async () => {
         if (!client.user || !client.application) return;
+        await client.application.commands.set(Commands);
         console.log(`${client.user.username} is online`);
     });
 };
