2d73e12d632f785ea0728208fbf68df1453c2f70
WhiteKr <hanjisu2215g@gmail.com>
2022-05-09 02:17:34 +0900

    feat: update handleSlashCommand

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -1,12 +1,21 @@
 import { BaseCommandInteraction, Client, Interaction } from "discord.js";
+import { Commands } from "../commands";
 
 export default (client: Client): void => {
-    client.on("interactionCreate", async (interaction: Interaction) => {
+    client.on('interactionCreate', async (interaction: Interaction) => {
         if (interaction.isCommand() || interaction.isContextMenu())
             await handleSlashCommand(client, interaction);
     });
 };
 
 const handleSlashCommand = async (client: Client, interaction: BaseCommandInteraction): Promise<void> => {
-    // handle slash command here
+    const slashCommand = Commands.find(c => c.name === interaction.commandName);
+    if (!slashCommand) {
+        interaction.followUp({ content: 'An error has occurred' });
+        return;
+    }
+
+    await interaction.deferReply();
+
+    slashCommand.run(client, interaction);
 };
