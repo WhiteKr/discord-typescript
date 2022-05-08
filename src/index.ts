c56c0adf62a7b62155a82c1c40e1f81ea11394d3
WhiteKr <hanjisu2215g@gmail.com>
2022-05-09 01:09:59 +0900

    feat: login bot

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Client } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 console.log('Bot is starting...');
 
 const client = new Client({
     intents: [],
 });
+client.login(process.env.BOT_TOKEN);
 
 console.log(client);
